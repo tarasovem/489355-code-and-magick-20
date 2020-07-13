45167b2df334646ed1e1c739384cbd443a5358b0
Evgeniy Tarasov <tarasovem@gmail.com>
2020-07-14 01:09:58 +0300

    Решение задания

---
+++ js/backend.js
@@ -0,0 +1,62 @@
+'use strict';
+
+(function () {
+  var URL_SAVE = 'https://javascript.pages.academy/code-and-magick';
+  var URL_LOAD = 'https://javascript.pages.academy/code-and-magick/dat';
+  var STATUS_CODE = {
+    OK: 200
+  };
+  var TIMEOUT_IN_MS = 10000;
+
+  window.backend = {
+    save: function (data, onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === STATUS_CODE.OK) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+
+      xhr.timeout = TIMEOUT_IN_MS;
+
+      xhr.open('POST', URL_SAVE);
+      xhr.send(data);
+    },
+    load: function (onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.open('GET', URL_LOAD);
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === STATUS_CODE.OK) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+
+      xhr.timeout = TIMEOUT_IN_MS;
+
+      xhr.send();
+    }
+  };
+})();
